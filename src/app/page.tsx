8095648db2df384e39499aab3e0c1b812541dba8
maodaisuki <nyanyanyapu@gmail.com>
2025-01-23 20:24:39 +0800

    上传备份

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Intro from "@/components/intro";
-import Item from "@/interfaces/item";
-import { getAllItems } from "@/lib/database";
-import { generatorQuizList } from "@/lib/quizGenerator";
 
 export default async function Home() {
-  const itemList: Array<Item> = await getAllItems();
-  const quizList = generatorQuizList(itemList, 10);
-  if(itemList == undefined || itemList.length == 0 || quizList == undefined || quizList.length == 0) {
-    return <></>
-  }
-  // 主页面
-  // 直接显示题目 :D
   return (
     <main className="m-0 min-h-screen min-w-screen flex flex-col items-center">
       <div className="md:w-full max-w-3xl flex flex-col items-center w-full min-h-screen ">
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,3 +1,12 @@
-export default function Quiz() {
+import Item from "@/interfaces/item";
+import { getAllItems } from "@/lib/database";
+import { generatorQuizList } from "@/lib/quizGenerator";
+
+export default async function Quiz() {
+    const itemList: Array<Item> = await getAllItems();
+    const quizList = generatorQuizList(itemList, 10);
+    if(itemList == undefined || itemList.length == 0 || quizList == undefined || quizList.length == 0) {
+        return <></>
+    }
     return <></>
 }
